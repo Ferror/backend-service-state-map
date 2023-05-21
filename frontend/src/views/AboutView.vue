ee9db287dfb78219fd3725742598d9c7501ea95a
Ferror <zmalcherczyk@gmail.com>
2023-05-22 00:00:06 +0200

    vue3-apexcharts

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,6 +1,36 @@
+<script>
+import VueApexCharts from "vue3-apexcharts";
+const options = {}
+
+export default {
+  data() {
+    return {
+      options: {
+        chart: {
+          id: 'vuechart-example'
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+        }
+      },
+      series: [{
+        name: 'series-1',
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }]
+    }
+  },
+  components: {
+    apexchart: VueApexCharts,
+  },
+}
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+  </div>
+  <div>
+    <apexchart width="500" type="bar" :options="options" :series="series"></apexchart>
   </div>
 </template>
 
